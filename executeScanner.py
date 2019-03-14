b47d11d32bf61fa84ad663919f04c141691166eb
Securityautomation <48516277+Securityautomation@users.noreply.github.com>
2019-03-14 17:54:08 +0530

    Update executeScanner.py
    Done the changes for Linux repository download and running SonarQube, SonarScanner and ElasticSearch

--- executeScanner.py
+++ executeScanner.py
@@ -1,7 +1,7 @@
 
 #Program to run SonarQube, SonarScanner and ElasticSearch which will scan a repo and provide results. 
 
-from sys import platform
+from sys
 from threading import Thread
 import os 
 import zipfile
@@ -16,23 +16,47 @@ import random
 import git
 import requests
 from subprocess import Popen
+import platform
 
 
 # Get the current directory where the script is running
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
+# ************************************************************************************
 # Function to run Elastic Search Mac OX
-def runElasticSearchUnix():
+def runElasticSearchMac():
 	subprocess.call("chmod -R 777 sonarQube",shell=True)
 	subprocess.call("./sonarQube/elasticsearch/bin/elasticsearch", shell=True)
 	time.sleep(8)
 
 # Function to run SonarQube Mac OX 
-def runSonarQubeUnix():
+def runSonarQubeMac():
 	subprocess.call("chmod -R 777 sonarQube", shell=True)
 	subprocess.call("./sonarQube/bin/macosx-universal-64/sonar.sh console", shell=True)
-	time.sleep(8)
+	time.sleep(8)	
 
+# ************************************************************************************	
+# Function to run Elastic Search Linux
+def runElasticSearchLinux():
+	subprocess.call("chmod -R 777 sonarQube",shell=True)
+	subprocess.call("./sonarQube/elasticsearch/bin/elasticsearch", shell=True)
+	time.sleep(8)	
+
+# Function to run SonarQube for Linux
+def runSonarQubeLinux():
+    bit = str(platform.architecture()[0])
+    if(bit=="64bit"):
+    	# This is path for StartSonar.bat file and will be run from here
+    	subprocess.call("chmod -R 777 sonarQube", shell=True)
+    	subprocess.call("./sonarQube/bin/linux-x86-64/sonar.sh console", shell=True)
+    elif(bit=="32bit"):
+    	# This is path for StartSonar.bat file and will be run from here
+    	subprocess.call("chmod -R 777 sonarQube", shell=True)
+    	subprocess.call("./sonarQube/bin/linux-x86-32/sonar.sh console", shell=True)
+    time.sleep(8)
+
+# ************************************************************************************	
+	
 # Function to run Elastic Search and SonarQube for Windows
 def runElasticSearchAndSonarWindows():
     bit = str(platform.architecture()[0])
@@ -46,8 +70,8 @@ def runElasticSearchAndSonarWindows():
     p = Popen(sonarQubePath)
     p.communicate()
     time.sleep(8)
-
-
+	
+# ************************************************************************************
 # Function to download the repository taken as an arguement
 # ******************************************************************************************************************
 
@@ -135,10 +159,11 @@ def downloadRepositoryWindows(url):
 	os.rename(projectPathWindows+"\\"+mname,projectPathWindows+"\\"+mname+tempTime)
 
 
+# ******************************************************************************************************************
 # Function to download the repository taken as an arguement
 # ******************************************************************************************************************
 
-def downloadRepositoryUnix(url):
+def downloadRepositoryLinux(url):
 	
 	# Snippet to find the filename from the URL
 	[url_first_part,mname]=url.rsplit('/', 1)
@@ -213,7 +238,83 @@ def downloadRepositoryUnix(url):
 	os.rename(path2+mname,path2+mname+tempTime)
 
 
+# ******************************************************************************************************************
+# Function to download the repository taken as an arguement
+# ******************************************************************************************************************
 
+def downloadRepositoryMac(url):
+	
+	# Snippet to find the filename from the URL
+	[url_first_part,mname]=url.rsplit('/', 1)
+	mname = mname.replace('.git','')
+
+	subprocess.call("mkdir -p sonarScanner/bin/project",shell=True)
+	# Path where you want to download the project
+	path2 = dir_path+'/sonarScanner/bin/project/'
+	
+	
+	print("\nDownloading Started ............ \n")  
+	# Cloning the repo into the path1 variable
+	git.Git(path2).clone(url)
+
+	print("\nThe repo has been cloned in our system .................... \n")
+
+	# Path of the properties file
+	propertiesFilePath = dir_path + '/sonarScanner/conf/sonar-scanner.properties'
+
+	# Reading the properties File and deleting all the contents in them
+
+	print("\nDeleting Old properties ................ \n")
+
+	f = open(propertiesFilePath, 'r+')
+	f.truncate(0)
+	data = f.readlines()
+
+	#  Appending the properties in the file one by one
+	data.append("sonar.sourceEncoding=UTF-8")
+	data.append("\n")
+	data.append("\n")
+	data.append("export SONAR_SCANNER_OPTS=-Xms512m\ -Xmx2048m")
+	data.append("\n")
+	data.append("\n")
+	data.append("sonar.projectKey="+mname)
+	data.append("\n")
+	data.append("\n")
+	data.append("sonar.projectName="+mname)
+	data.append("\n")
+	data.append("\n")
+	data.append("sonar.projectVersion=1.0")
+	data.append("\n")
+	data.append("\n")
+	data.append("sonar.scm.disabled=true")
+	data.append("\n")
+	data.append("\n")
+
+	#data.append("sonar.nodejs.executable=/usr/local/n/versions/node/11.6.0")
+	
+	data.append("sonar.sources="+dir_path+"/sonarScanner/bin/project/"+mname)
+	data.append("\n")
+	data.append("\n")
+	data.append("sonar.java.binaries="+dir_path+"/sonarScanner/bin/project/"+mname)
+
+	# and write everything back
+	with open(propertiesFilePath, 'w') as file:
+		file.writelines(data)
+
+	# Running the command for scanning the files
+
+	# Changing the mod for sonarScanner in Unix 
+
+	print("\nChanging the mode of sonarScanner directory ............. \n")
+	subprocess.call("chmod -R 777 sonarScanner",shell=True)
+	bashCommand = './sonarScanner/bin/sonar-scanner'
+
+	print("\nRunning sonarScanner.sh file.\nScanning of " + mname + " repo started ................. \n")
+	# Running the sonarScanner in Unix
+	process = subprocess.call(bashCommand, shell=True)
+	
+	tempTime = str(datetime.datetime.now())
+	os.rename(path2+mname,path2+mname+tempTime)
 
 # Function to start the Elastic Search, SonarQube and Download repositories and run sonarScanner in different threads
 #***********************************************************************************************************************
@@ -234,26 +335,25 @@ if __name__ == '__main__':
 
 
 	# Checking the OS of the user's system
-	if platform=="darwin":
+	if sys.platform=="darwin":
 		print("Mac OS recognised ... \n")	
-		Thread(target=runElasticSearchUnix).start()
+		Thread(target=runElasticSearchMac).start()
 		time.sleep(8)
-		Thread(target=runSonarQubeUnix).start()
+		Thread(target=runSonarQubeMac).start()
 		time.sleep(8)
-		Thread(target=downloadRepositoryUnix, args=(url,)).start()
+		Thread(target=downloadRepositoryMac, args=(url,)).start()
 
-	elif platform=="win32" or platform=="win64"  or "win" in platform:
+	elif sys.platform=="win32" or sys.platform=="win64"  or "win" in sys.platform:
 		print("Windows recognised ... \n")
 		Thread(target=runElasticSearchAndSonarWindows).start()
 		time.sleep(8)
 		Thread(target=downloadRepositoryWindows,  args=(url,)).start()
 
-
-	elif platform=="linux1" or platform=="linux2":
+	elif sys.platform=="linux1" or sys.platform=="linux2" or sys.platform=="linux":
 		print("Linux recognised ... \n")	
-		Thread(target=runElasticSearchUnix).start()
+		Thread(target=runElasticSearchLinux).start()
 		time.sleep(8)
-		Thread(target=runSonarQubeUnix).start()
+		Thread(target=runSonarQubeLinux).start()
 		time.sleep(8)
-		Thread(target=downloadRepositoryUnix, args=(url,)).start()
+		Thread(target=downloadRepositoryLinux, args=(url,)).start()
 	
